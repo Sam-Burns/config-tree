463b76285d68620a3039eaa61ffb562182a7b964
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-04 19:45:37 +0100

    Giving constructor to builder

--- src/ConfigTree/Builder/ConfigTreeBuilder.php
+++ src/ConfigTree/Builder/ConfigTreeBuilder.php
@@ -2,9 +2,21 @@
 namespace ConfigTree\Builder;
 
 use ConfigTree\Tree\ConfigTree;
+use ConfigTree\FileParsing\ArrayableFileFactory;
 
 class ConfigTreeBuilder
 {
+    /** @var ArrayableFileFactory */
+    private $arrayableFileFactory;
+
+    /**
+     * @param ArrayableFileFactory|null $arrayableFileFactory
+     */
+    public function __construct(ArrayableFileFactory $arrayableFileFactory = null)
+    {
+        $this->arrayableFileFactory = $arrayableFileFactory ?: new ArrayableFileFactory();
+    }
+
     /**
      * @param string $pathToFile
      */
