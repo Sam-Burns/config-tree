0644de061f5178f879c0034f69dde3549dd8991c
Sam-Burns <sam.burns.uk@gmail.com>
2015-07-04 21:01:40 +0100

    Adding integration tests for file parsing

---
+++ tests/fixtures/file_formats/config.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'node1' => array(
+        'node2' => 'value'
+    )
+);
---
+++ tests/phpunit/ConfigTree/Test/ReadingFiles/ReadingVariousFileFormatTest.php
@@ -0,0 +1,42 @@
+<?php
+namespace ConfigTree\Test\ReadingFiles;
+
+use ConfigTree\FileParsing\ArrayableFileFactory;
+use PHPUnit_Framework_TestCase as TestCase;
+
+class ReadingVariousFileFormatTest extends TestCase
+{
+    /**
+     * @dataProvider provideFilenames
+     *
+     * @param string $filename
+     * @param string $errorMessage
+     */
+    public function testReadingFileContents($filename, $errorMessage)
+    {
+        $this->markTestSkipped();
+
+        // ARRANGE
+        $arrayableFileFactory = new ArrayableFileFactory();
+        $arrayableFile = $arrayableFileFactory->getArrayableFileFromPath(
+            TEST_DIR . '/fixtures/file_formats/' . $filename
+        );
+        $expectedArray = ['node1' => ['node2' => 'value']];
+
+        // ACT
+        $arrayReadFromFile = $arrayableFile->toArray();
+
+        // ASSERT
+        $this->assertEquals($expectedArray, $arrayReadFromFile, $errorMessage);
+    }
+
+    public function provideFilenames()
+    {
+        return [
+            ['config.ini',  'Error parsing .ini config file'],
+            ['config.json', 'Error parsing .json config file'],
+            ['config.php',  'Error parsing .php config file'],
+            ['config.yml',  'Error parsing .yml config file'],
+        ];
+    }
+}
